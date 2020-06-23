353d6b14be2cc4ad3040b1b5149808eeb6219197
ryuji <ryu.seven.h7420@gmail.com>
2020-06-23 22:33:25 +0900

    ホーム画面(仮)

--- todos/src/components/App.js
+++ todos/src/components/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from './Header';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="ui container">
+      <Header />
+      <button className="ui button">ログイン</button>
+      <button className="ui button">新規登録</button>
+    </div>
+  );
 };
 
 export default App;
---
+++ todos/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="ui secondary pointing menu">
+      <h1>Todo</h1>
+      <div className="right menu">sign in</div>
+    </div>
+  );
+};
+
+export default Header;
