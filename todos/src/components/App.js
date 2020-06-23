92b8f418163f89d3b7faa1c2d747f4c52c582c7c
ryuji <ryu.seven.h7420@gmail.com>
2020-06-24 07:48:37 +0900

    新規登録、ログイン画面へのページ遷移

--- todos/src/components/App.js
+++ todos/src/components/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
 import Header from './Header';
+import MainPage from './pages/MainPage';
+import SignUp from './ authentication/SignUp';
+import SignIn from './ authentication/SignIn';
+import history from '../history';
 
 const App = () => {
   return (
     <div className="ui container">
-      <Header />
-      <button className="ui button">ログイン</button>
-      <button className="ui button">新規登録</button>
+      <Router history={history}>
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={MainPage} />
+            <Route path="/signup" exact component={SignUp} />
+            <Route path="/signin" exact component={SignIn} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 };
--- todos/src/components/Header.js
+++ todos/src/components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className="ui secondary pointing menu">
-      <h1>Todo</h1>
-      <div className="right menu">sign in</div>
+      <Link to="/" className="item">
+        ToDo
+      </Link>
+      <div className="right menu">
+        <Link to="/signin" className="ui button">
+          Sign In
+        </Link>
+        <Link to="/signup" className="ui button">
+          Sign Up
+        </Link>
+      </div>
     </div>
   );
 };
