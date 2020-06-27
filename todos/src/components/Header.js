021eb15bc84dead3495f212ff9b98ec43a9aaf0c
ryuji <ryu.seven.h7420@gmail.com>
2020-06-27 16:22:27 +0900

    ヘッダーの表示方法変更(ログイン状態を参照して変更)

--- todos/src/components/Header.js
+++ todos/src/components/Header.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { signOut } from '../actions';
 
-const Header = () => {
-  //あとで消す -> sign outの処理はactionに
+const Header = (props) => {
+  // ログアウト処理
   const handleSignOut = () => {
     sessionStorage.clear();
+    props.signOut();
+  };
+
+  // ログイン状態によってボタンの表示を変更
+  const renderAuthButton = () => {
+    if (props.isSignedIn) {
+      return (
+        <Link to="/" className="ui button" onClick={handleSignOut}>
+          Sign Out
+        </Link>
+      );
+    } else {
+      return (
+        <React.Fragment>
+          <Link to="/signin" className="ui button">
+            Sign In
+          </Link>
+          <Link to="/signup" className="ui button">
+            Sign Up
+          </Link>
+        </React.Fragment>
+      );
+    }
   };
 
   return (
@@ -12,19 +37,13 @@ const Header = () => {
       <Link to="/" className="item">
         ToDo
       </Link>
-      <div className="right menu">
-        <Link to="/signin" className="ui button">
-          Sign In
-        </Link>
-        <Link to="/signup" className="ui button">
-          Sign Up
-        </Link>
-        <Link to="/" className="ui button" onClick={handleSignOut}>
-          Sign Out
-        </Link>
-      </div>
+      <div className="right menu">{renderAuthButton()}</div>
     </div>
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+  return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(mapStateToProps, { signOut })(Header);
