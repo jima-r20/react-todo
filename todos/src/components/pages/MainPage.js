587c4a42dfb2bef9185e6d5563935451d55ad2b6
ryuji <ryu.seven.h7420@gmail.com>
2020-07-05 02:24:47 +0900

    ログイン前のメインページのデザイン変更

--- todos/src/components/pages/MainPage.js
+++ todos/src/components/pages/MainPage.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const MainPage = () => {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Welcome!!</h1>
+    <div style={{ textAlign: 'center', paddingTop: '25vh' }}>
+      <div
+        style={{
+          fontSize: '10vh',
+          fontWeight: 'bold',
+          lineHeight: '100%',
+          color: '#48834C',
+        }}
+      >
+        Welcome to TODO App!!
+      </div>
     </div>
   );
 };
