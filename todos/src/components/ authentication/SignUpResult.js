578cf6fdd006ae85296a80ebc40e651033d56991
ryuji <ryu.seven.h7420@gmail.com>
2020-07-05 15:01:56 +0900

    signup Result画面のUI変更

--- todos/src/components/ authentication/SignUpResult.js
+++ todos/src/components/ authentication/SignUpResult.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 const signUpResult = () => {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Your Sign Up is Completed !!!</h1>
+    <div style={{ textAlign: 'center', color: '#48834C', paddingTop: '10vh' }}>
+      <h1 style={{ fontSize: '10vh' }}>Your Sign Up is Completed !!!</h1>
       <div>
-        <h3>Do you want to sign in ?</h3>
-        <Link to="/signin" className="ui button primary">
+        <h3 style={{ fontSize: '5vh' }}>Do you want to sign in ?</h3>
+        <Link to="/signin" className="ui large button primary">
           YES
         </Link>
-        <Link to="/" className="ui button">
+        <Link to="/" className="ui large button black">
           no
         </Link>
       </div>
